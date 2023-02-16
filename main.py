f7aaf14b4ced397d338e40160aad46386d359528
dhruvVIP7 <106507462+dhruvVIP7@users.noreply.github.com>
2023-02-16 15:37:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+import os
+from discord.ext import commands
+
+client=commands.bot(command_prefix="your prefix here")
+
+@client.event
+async def on_ready():
+  print("Your bot is online!")
+
+client.run(os.environ['token'])
